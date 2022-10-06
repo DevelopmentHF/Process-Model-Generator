ea4d7eafd72e014b9f35b659cdf91dbe53a1a4e1
Henry Fielding <development.henry@gmail.com>
2022-10-06 18:55:04 +1100

    Add files via upload

--- main.c
+++ main.c
@@ -55,6 +55,7 @@ typedef unsigned int action_t;  // an action is identified by an integer
 typedef struct event event_t;   // an event ...
 struct event {                  // ... is composed of ...
     char actn;              // ... an action that triggered it and ...
+    int freq;                   // num times that this event appeared (only used sometimes)
     event_t* next;              // ... a pointer to the next event in the trace
 };
 
@@ -68,21 +69,29 @@ typedef struct {                // an event log is an array of distinct traces
                                 //     sorted lexicographically (alphabetically? check this henry.)
     trace_t** traces;            // an array of pointers to traces
     int      num_distinct;      // the number of distinct traces in this log
-    int      capacity;          // the capacity of this event log as the number
+    char*    distinct_events;
+    int      num_events;          // the capacity of this event log as the number
+    int*     max_freq_traces; // linked list of trace indices in the array of traces that have max freq
                                 //     of  distinct traces it can hold
 } log_t;
 
 typedef action_t** DF_t;        // a directly follows relation over actions // 2d array of actions
 
+
+
 /* PROTOTYPES ----------------------------------------------------------------*/
 trace_t* trace_build();
 void log_build(log_t* input_log);
+int trace_cmp(trace_t* t1, trace_t* t2);
 trace_t* make_empty_trace(void);
 trace_t* insert_at_foot(trace_t* trace, char letter);
 int is_trace_present(trace_t* trace, trace_t** list, int num_traces, int* index);
 int is_empty_trace(trace_t* trace);
 int find_num_events(log_t* log);
 int get_trace_len(trace_t* trace);
+void get_event_frequencies(log_t* log);
+int trace_num_details(log_t* log, int* max);
+void print_trace(log_t* log, int max_indice);
 void stage0_printer(log_t* log);
 
 /* WHERE IT ALL HAPPENS ------------------------------------------------------*/
@@ -111,8 +120,6 @@ trace_build() {     // remove this argyment and put it in the log_build()
             insert_at_foot(trace, letter);
 
         } else if (event != ','){
-            // printf("\nHead: %c, Foot: %c\n", trace->head->actn, trace->foot->actn);
-            // printf("\n");
             // return an int to signal to call again in main while loop
             return trace;
         }
@@ -148,9 +155,7 @@ log_build(log_t* input_log) {
         if (found_flag == NOT_FOUND) {
 
             cur_trace->freq=1;
-            // printf("NOT SEEN\n");
-            // printf("cur freq: %d\n\n", cur_trace->freq);
-            // Check if malloc'd list is big enough
+            
             if (num_distinct == current_size) {
                 current_size *= 2;
                 trace_list = (trace_t**)realloc(trace_list, current_size*sizeof(*trace_list));
@@ -159,15 +164,25 @@ log_build(log_t* input_log) {
 
             trace_list[num_distinct] = (trace_t*)malloc(sizeof(trace_t*));
             assert(trace_list[num_distinct]);
-            trace_list[num_distinct] = cur_trace;
-            num_distinct++;
+            trace_list[num_distinct++] = cur_trace;
+            // Do insertion sort here -----------------
+            if (num_distinct > 1) {
+                for (int i=num_distinct-1; i>0; i--) {
+                    // change the guard to compare two linked lists
+                    if (trace_cmp(trace_list[i], trace_list[i-1]) == -1) {
+                        // swap the two 
+                        trace_t* temp = trace_list[i];
+                        trace_list[i] = trace_list[i-1];
+                        trace_list[i-1] = temp;
+                        printf("swapped");
+                    } else {
+                        break;
+                    }
+                }
+            }
 
         } else if (found_flag == FOUND) {
-            // if it is found, need to change frquency of struct object from BEFORE
-            // cur_trace->freq;
             trace_list[found_index]->freq++;
-            // printf("ALREADY SEEN\n");
-            // printf("cur freq: %d\n\n", trace_list[found_index]->freq);
         } 
 
         // Incremental operations
@@ -179,13 +194,34 @@ log_build(log_t* input_log) {
     // input_log->capacity
 }
 
+int
+trace_cmp(trace_t* t1, trace_t* t2) {
+    event_t* cur_event1 = t1->head;
+    event_t* cur_event2 = t2->head;
+
+    // traverses the LL's and checks
+    while (cur_event1->next != NULL || cur_event2->next != NULL) {
+        if(cur_event1->actn > cur_event2->actn) {
+            return 1;
+        } else if (cur_event1->actn < cur_event2->actn) {
+            return -1;
+        } else {
+            cur_event1 = cur_event1->next;
+            cur_event2 = cur_event2->next;
+        }
+    }
+    // One more check for final nodes
+    if(cur_event1->actn > cur_event2->actn) {
+        return 1;
+    } else if (cur_event1->actn < cur_event2->actn) {
+        return -1;
+    } 
+    return 0;
+}
+
+
 /* make_empty_trace() modified from Alistair Moffat, as an example for the book
-   "Programming, Problem Solving, and Abstraction with C", Pearson
-   Custom Books, Sydney, Australia, 2002; revised edition 2012,
-   ISBN 9781486010974.
-   See http://people.eng.unimelb.edu.au/ammoffat/ppsaa/ for further
-   information.
-   Prepared December 2012 for the Revised Edition
+   "Programming, Problem Solving, and Abstraction with C"
 */
 trace_t*
 make_empty_trace(void) {
@@ -198,12 +234,7 @@ make_empty_trace(void) {
 
 
 /* insert_at_foot() modified from Alistair Moffat, as an example for the book
-   "Programming, Problem Solving, and Abstraction with C", Pearson
-   Custom Books, Sydney, Australia, 2002; revised edition 2012,
-   ISBN 9781486010974.
-   See http://people.eng.unimelb.edu.au/ammoffat/ppsaa/ for further
-   information.
-   Prepared December 2012 for the Revised Edition
+   "Programming, Problem Solving, and Abstraction with C"
 */
 trace_t*
 insert_at_foot(trace_t* trace, char letter) {
@@ -286,7 +317,6 @@ find_num_events(log_t* log) {
         // loop thru each trace
         int end_flag = 0;
         while(cur_event->next!=NULL || end_flag == 1) {
-            // printf("%d loop. char = %c\n", i, cur_event->actn);
             // Checks if char is present in found array of chars
             // no null byte > not a string but simply array of chars
             // can optimise but dont yet
@@ -311,6 +341,7 @@ find_num_events(log_t* log) {
                 event_list[num_distinct_events++] = cur_event->actn;
             }
 
+            // runs one more time if at the end of the linked list (i think)
             if (end_flag == 1) {
                 break;
             } else {
@@ -321,6 +352,8 @@ find_num_events(log_t* log) {
             }
         }   
     }
+    log->num_events = num_distinct_events;
+    log->distinct_events = event_list;
     return num_distinct_events;
 }
 
@@ -347,21 +380,69 @@ get_trace_len(trace_t* trace) {
     return sum;
 }
 
+
+void
+get_event_frequencies(log_t* log) {
+    // for finding how many times each event happens
+    // could use the event list created in find_num_events() -> make sure its free'd after use as well? maybe.
+    // could then sort it alpabetically to get print order correct
+    // then.. find how many times each event appears in a distinct trace
+    // multiply by frequency 
+    for (int i=0; i<log->num_events; i++) {
+        printf("Distinct Letter [%d]: %c\n", i, log->distinct_events[i]);
+        // Now check each trace for how many times its present
+        
+    }
+}
+
+int
+trace_num_details(log_t* log, int* max) {
+    int sum = 0, max_freq = 0;
+    for (int i=0; i<log->num_distinct; i++) {
+        int cur_freq = log->traces[i]->freq;
+        printf("cur freq = %d    max freq = %d\n", cur_freq, max_freq);
+        if (cur_freq > max_freq) {
+            max_freq = cur_freq;
+            // SEE NOTE AT BOTTOM
+            *max = max_freq;
+        }
+        // printf("freq of this = %d", log->traces[2]->freq);
+        sum += cur_freq;
+    }
+    return sum;
+}
+
+void
+print_trace(log_t* log, int indice) {
+    trace_t* max_freq_trace = log->traces[indice];
+    // Traverse the linked list
+    event_t* cur_event = max_freq_trace->head;
+    while(cur_event->next != NULL) {
+        printf("%c", cur_event->actn);
+        cur_event = cur_event->next;
+    }
+    printf("%c", cur_event->actn);
+    printf("\n");
+    
+}
+
 void
 stage0_printer(log_t* log) {
     /* Print Details */
+    int max_trace = 0;
     printf("==STAGE 0============================\n");
     printf("Number of distinct events: %d\n", find_num_events(log));
     printf("Number of distinct traces: %d\n", log->num_distinct);
     printf("Total number of events: %d\n", total_events(log));
-
+    printf("Total number of traces: %d\n", trace_num_details(log, &max_trace));
+    printf("Most frequent trace frequency: %d\n", max_trace);
+    // GO through array of LL's. if it has max freq, print it
+    for (int i=0; i<log->num_distinct; i++) {
+        if (log->traces[i]->freq == max_trace) {
+            print_trace(log, i);
+        }
+    }
+    // now find quantities of each event
+    get_event_frequencies(log);
 }
-
-// for finding how many times each event happens
-// could use the event list created in find_num_events()
-// could then sort it alpabetically to get print order correct
-
-// then.. find how many times each event appears in a distinct trace
-// multiply by frequency > add to linked list in form |EVENT & FREQUENCY| |POINTER -->| etc
-
 
